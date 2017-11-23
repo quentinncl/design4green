8e28fa7722e6277dd5127b1cc30936b6cc1eaed1
Mélanie GEFFRE <blooue635@gmail.com>
2017-11-23 15:09:35 +0100

    form select menu specialty

--- src/components/form.js
+++ src/components/form.js
@@ -7,12 +7,38 @@ import {createDentists} from '../actions/index';
 
 
 class FormDentists extends Component {
+    constructor(props){
+        super(props);
+
+        this.renderOptions = this.renderOptions.bind(this);
+        this.renderField = this.renderField.bind(this);
+    }
+
+    renderOptions(){
+        const test = [
+            {id:"0", value:"Pediatric Dentistry"},
+            {id:"1", value:"Unknown"},
+            {id:"2",value:"Oral and Maxillofacial Radiology"},
+            {id:"3",value:"Endodontics"},
+            {id:"4",value:"Dental Public Health"},
+            {id:"5",value:"Periodontics"},
+            {id:"6",value:"Oral and Maxillofacial Pathology"},
+            {id:"7",value:"Oral and Maxillofacial Surgery"},
+            {id:"8",value:"Prosthodontics"},
+            {id:"9",value:"Orthodontics and Dentofacial Orthopedics"}
+        ];
+
+        return test.map((specialty)=>{
+            console.log(specialty);
+             return <option>Plop</option>
+        });
+    }
 
     renderField(field) {
 
         const {meta: {touched, error}} = field;
         const className = `form-group ${touched && error ? 'has-danger' : ''}`;
-        console.log(field.label);
+
         if (field.label=='Speciality'){
             return (
                 <div className={className}>
@@ -20,10 +46,10 @@ class FormDentists extends Component {
                     <select className="form-control custom-select">
                         <option selected>Select a speciality</option>
                         <option value="0">Pediatric Dentistry</option>
-                        <option value="2">Oral and Maxillofaciel Radiology</option>
+                        <option value="2">Oral and Maxillofacial Radiology</option>
                         <option value="3">Endodontics</option>
                         <option value="4">Dental Public Health</option>
-                        <option value="5">Peridontics</option>
+                        <option value="5">Periodontics</option>
                         <option value="6">Oral and Maxillofacial Pathology</option>
                         <option value="7">Oral and Maxillofacial Surgery</option>
                         <option value="8">Prosthodontics</option>
@@ -51,6 +77,7 @@ class FormDentists extends Component {
     }
 
     /* OPTIMISATION */
+    /*
     renderOptions() {
         return (
             this.state.days.map(function(day) {
@@ -58,6 +85,7 @@ class FormDentists extends Component {
             })
         )
     }
+    */
     /****************/
 
     render() {
