5512164fdb5e976cb21749a2e3624b927e9500eb
Mélanie GEFFRE <blooue635@gmail.com>
2017-11-24 09:58:10 +0100

    test pour cross origin

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,27 @@ const ROOT_URL = 'http://51.254.37.175:5001/api';
 
 export function fetchDentistsLastName(lastname){
 
-    const request = axios.get(`${ROOT_URL}/dentists/last_name/${lastname.lastname}`);
+    const request = axios.get(`${ROOT_URL}/dentists/last_name/${lastname.lastname}`, {
+        headers: {
+            'Access-Control-Allow-Origin':'*',
+        },
+        proxy: {
+            host: '51.254.37.175',
+            port: 8181
+        }}).then(function (response) {
+        console.log('response is : ' + response.data);
+    }).catch(function (error) {
+        if (error.response) {
+            console.log(error.response.headers);
+        }
+        else if (error.request) {
+            console.log(error.request);
+        }
+        else {
+            console.log(error.message);
+        }
+        console.log(error.config);
+    });
 
     return {
         type: FETCH_DENTISTS_LASTNAME,
